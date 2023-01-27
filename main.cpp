12a0c3d4cb221485c5e102b9ea24145894819a8c
Shreyas-Nagabhushan <110516607+Shreyas-Nagabhushan@users.noreply.github.com>
2023-01-27 10:45:28 +0530

    victory message

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include<iostream>
 #include"includes/header.h"
 #include"includes/header2.h"
+#define repfor(i, a, b) for(i=a;i<b;i++)
+#define repbac(i, a, b) for(i=a;i>=b;i--)
+typedef long long ll ;
+
 int main()
 {
+    int i ;
+    repfor(i ,0, 10){
+        std::cout<<"keyboard dragons ROCKS!!"<<std::endl ;
+    }
     std::cout<<add(2,3)<<"\n";
     getchar();
+    return 0 ;
 }

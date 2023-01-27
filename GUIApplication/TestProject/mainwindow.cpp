3ad3ed887a83cc0b6cc2a0999e20e8c490891917
VaibhavRaina <122040517+VaibhavRaina@users.noreply.github.com>
2023-01-27 12:08:16 +0530

    GUI

---
+++ GUIApplication/TestProject/mainwindow.cpp
@@ -0,0 +1,22 @@
+#include "mainwindow.h"
+#include<process.h>
+#include "./ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    exit(0);
+}
+
